3512825f967f5b7e8dc2ffc61d3913506982dd00
Bogdan <win2win@protonmail.com>
2018-02-26 08:56:06 -0600

    test

--- ico.sol
+++ ico.sol
@@ -44,7 +44,7 @@ contract ERC20 {
 /**
  * @title Ownable
  * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
+ * functions, this simplifies the implementation of "user permissions"...
  */
 contract Ownable {
 
